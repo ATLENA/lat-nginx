7a82e2b80ba1cafaf2a5236b8a4709f8c959f9ec
서세진 책임/SWA Innovation팀 <sjsj@lgcns.com>
2023-05-23 22:10:25 +0900

    feat: nginx engine and template packaging #1
    WAS_USER -> RUN_USER

--- nginx-template/1.23/base/env.sh
+++ nginx-template/1.23/base/env.sh
@@ -11,7 +11,7 @@ export LOG_HOME=/apps/lat/1.0.0/instances/lat-was-8080/logs
 export ENGN_VERSION=1.23.1.A.RELEASE
 export ENGN_HOME=${LAT_HOME}/engines/runtime/nginx/nginx-${ENGN_VERSION}
 
-export WAS_USER=lat
+export RUN_USER=lat
 export INST_NAME=${INSTANCE_ID}_`hostname`
 
 export NGINX_PID=${CATALINA_BASE}/${INSTANCE_ID}.pid
