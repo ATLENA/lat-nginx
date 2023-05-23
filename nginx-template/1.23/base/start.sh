aeb00e2dbf03da17022c0bddbdc3ee149f2555cb
서세진 책임/SWA Innovation팀 <sjsj@lgcns.com>
2023-05-23 19:22:08 +0900

    feat: nginx engine and template packaging #1
    start, stop file modified

--- nginx-template/1.23/base/start.sh
+++ nginx-template/1.23/base/start.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/enlena/*RELEASE/bin/latctl.sh start nginx ${INSTANCE_ID}
+${LAT_HOME}/management/latctl/bin/latctl.sh start nginx ${INSTANCE_ID}
 
--- nginx-template/1.23/base/stop.sh
+++ nginx-template/1.23/base/stop.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/enlena/*RELEASE/bin/latctl.sh stop nginx ${INSTANCE_ID}
+${LAT_HOME}/management/latctl/bin/latctl.sh stop nginx ${INSTANCE_ID}
 
